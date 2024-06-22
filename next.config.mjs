bcfa1affa6562e267f7334ff08f4c689967d5be1
Matt Kaye <mrkaye97@gmail.com>
2024-06-22 16:03:46 -0400

    permanent

--- next.config.mjs
+++ next.config.mjs
@@ -11,15 +11,18 @@ const nextConfig = {
         source:
           "/posts/2023-06-29-lessons-learned-from-running-r-in-production/lessons-learned-from-running-r-in-production.html",
         destination: "/blog/lessons-learned-from-running-r-in-production",
+        permanent: false,
       },
       {
         source: "/posts/2023-11-10-jobcrawler/jobcrawler.html",
         destination: "/blog/introducing-zensearch",
+        permanent: false,
       },
       {
         source:
           "/posts/2023-07-09-unit-testing-dbt-models/unit-testing-dbt-models.html",
         destination: "/blog/unit-testing-dbt-models",
+        permanent: false,
       },
     ];
   },
