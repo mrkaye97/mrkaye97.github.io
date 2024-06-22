8bb132ee1b10ad423c8f043e9dda65b7632e0072
Matt Kaye <mrkaye97@gmail.com>
2024-06-22 16:10:58 -0400

    redirects dont work in export mode apparently

--- next.config.mjs
+++ next.config.mjs
@@ -5,27 +5,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  async redirects() {
-    return [
-      {
-        source:
-          "/posts/2023-06-29-lessons-learned-from-running-r-in-production/lessons-learned-from-running-r-in-production.html",
-        destination: "/blog/lessons-learned-from-running-r-in-production",
-        permanent: false,
-      },
-      {
-        source: "/posts/2023-11-10-jobcrawler/jobcrawler.html",
-        destination: "/blog/introducing-zensearch",
-        permanent: false,
-      },
-      {
-        source:
-          "/posts/2023-07-09-unit-testing-dbt-models/unit-testing-dbt-models.html",
-        destination: "/blog/unit-testing-dbt-models",
-        permanent: false,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
