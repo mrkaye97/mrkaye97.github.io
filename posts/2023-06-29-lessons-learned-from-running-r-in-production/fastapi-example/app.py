cca91da3127c12019e15ad3e52e445a7e10c5693
Matt Kaye <mrkaye97@gmail.com>
2024-06-22 15:17:31 -0400

    nuke old site

--- posts/2023-06-29-lessons-learned-from-running-r-in-production/fastapi-example/app.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/types")
-async def types(n: int) -> int:
-  return n * 2
--- posts/2023-06-29-lessons-learned-from-running-r-in-production/plumber-types-example/app.R
+++
@@ -1,5 +0,0 @@
-library(plumber)
-
-api <- plumb("posts/2023-06-29-lessons-learned-from-running-r-in-production/plumber-types-example/plumber.R")
-
-api$run()
