7f0a46915afe1379029e6e7bcedad722be7b92f2
Matt Kaye <mrkaye97@gmail.com>
2024-06-24 21:13:30 -0400

    code page

--- pages/code.tsx
+++ pages/code.tsx
@@ -1,46 +1,61 @@
 import { TextLink } from "@/src/components/links";
 import React from "react";
 
+function CodeCard({
+  title,
+  children,
+}: {
+  title: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="bg-opacity-100 border border-gray-500 shadow-2xl rounded-lg p-6 m-4">
+      <h1 className="text-3xl font-bold text-white mb-4">{title}</h1>
+      {children}
+    </div>
+  );
+}
+
 export default function Code() {
   return (
     <div className="max-w-3xl mx-auto px-4 py-8">
-      <p className="text-lg text-white mb-4">
-        I like writing code for fun, to explore interesting questions and
-        problems, to learn, and to improve the tools I use.
-      </p>
+      <CodeCard title={"Introduction"}>
+        <p className="text-lg text-white mb-4">
+          I like writing code for fun, to explore interesting questions and
+          problems, to learn, and to improve the tools I use.
+        </p>
 
-      <p className="text-lg text-white mb-4">
-        I usually have at least one or two things I&apos;m toying around with at
-        any given time, whether it&apos;s{" "}
-        <TextLink
-          text="an API wrapper for working with my Fitbit data"
-          href="https://github.com/mrkaye97/fitbitr"
-        />
-        , attempting{" "}
-        <TextLink
-          text="Euler"
-          href="https://github.com/mrkaye97/Haskell-Euler"
-        />{" "}
-        or{" "}
-        <TextLink
-          text="Advent of Code"
-          href="https://github.com/mrkaye97/advent-of-code-2023"
-        />{" "}
-        problems, working on{" "}
-        <TextLink
-          text="this website"
-          href="https://github.com/mrkaye97/mrkaye97.github.io"
-        />
-        , or building{" "}
-        <TextLink
-          text="better job searching tools"
-          href="https://zensearch.jobs"
-        />
-        .
-      </p>
-
-      <div className="my-8">
-        <h2 className="text-3xl font-bold text-white mb-2">ZenSearch</h2>
+        <p className="text-lg text-white mb-4">
+          I usually have at least one or two things I&apos;m toying around with
+          at any given time, whether it&apos;s{" "}
+          <TextLink
+            text="an API wrapper for working with my Fitbit data"
+            href="https://github.com/mrkaye97/fitbitr"
+          />
+          , attempting{" "}
+          <TextLink
+            text="Euler"
+            href="https://github.com/mrkaye97/Haskell-Euler"
+          />{" "}
+          or{" "}
+          <TextLink
+            text="Advent of Code"
+            href="https://github.com/mrkaye97/advent-of-code-2023"
+          />{" "}
+          problems, working on{" "}
+          <TextLink
+            text="this website"
+            href="https://github.com/mrkaye97/mrkaye97.github.io"
+          />
+          , or building{" "}
+          <TextLink
+            text="better job searching tools"
+            href="https://zensearch.jobs"
+          />
+          .
+        </p>
+      </CodeCard>
+      <CodeCard title="ZenSearch">
         <p className="text-lg text-white mb-4">
           <TextLink text="ZenSearch" href="https://zensearch.jobs" /> is a
           NextJS / FastAPI app I&apos;ve been building to make your job hunt
@@ -61,32 +76,30 @@ export default function Code() {
           and{" "}
           <TextLink text="try it out for free" href="https://zensearch.jobs" />.
         </p>
-      </div>
-
-      <div className="mb-8">
-        <h2 className="text-3xl font-bold text-white mb-2">
-          Open-Source Contributions
-        </h2>
-        <p className="text-lg text-white mb-4">
-          I&apos;m a big believer in open-source, and like to contribute to
-          projects - aside from my own - that I use and love when I get a
-          chance. Some contributions I&apos;m proud of include:
-        </p>
-        <ul className="list-disc pl-6">
-          <li className="text-lg text-white mb-2">
-            <TextLink
-              text="Adding JSON schema validation to MLFlow"
-              href="https://github.com/mlflow/mlflow/pull/5458"
-            />
-          </li>
-          <li className="text-lg text-white mb-2">
-            <TextLink
-              text="Adding a MAP@K implementation to `recmetrics`"
-              href="https://github.com/statisticianinstilettos/recmetrics/pull/50"
-            />
-          </li>
-        </ul>
-      </div>
+      </CodeCard>
+      <CodeCard title="Open-Source">
+        <div className="mb-8">
+          <p className="text-lg text-white mb-4">
+            I&apos;m a big believer in open-source, and like to contribute to
+            projects - aside from my own - that I use and love when I get a
+            chance. Some contributions I&apos;m proud of include:
+          </p>
+          <ul className="list-disc pl-6">
+            <li className="text-lg text-white mb-2">
+              <TextLink
+                text="Adding JSON schema validation to MLFlow"
+                href="https://github.com/mlflow/mlflow/pull/5458"
+              />
+            </li>
+            <li className="text-lg text-white mb-2">
+              <TextLink
+                text="Adding a MAP@K implementation to `recmetrics`"
+                href="https://github.com/statisticianinstilettos/recmetrics/pull/50"
+              />
+            </li>
+          </ul>
+        </div>
+      </CodeCard>
     </div>
   );
 }
