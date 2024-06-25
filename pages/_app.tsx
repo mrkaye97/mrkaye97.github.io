b227a0dc7ea37f004561e41690225a16674ce724
Matt Kaye <mrkaye97@gmail.com>
2024-06-24 21:39:28 -0400

    add titles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,19 +5,53 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 import Layout from "./layout";
 import { GoogleAnalytics } from "nextjs-google-analytics";
 import Head from "next/head";
+import { usePathname } from "next/navigation";
 
 config.autoAddCss = false;
 
+function createPathAddendum(path: string) {
+  switch (path) {
+    case "/":
+      return "Home";
+    case "/my-three-favorite":
+      return "My Three Favorite";
+    case "/code":
+      return "Code";
+    case "/blog":
+      return "Blog";
+    case "/travel":
+      return "Travel";
+    default:
+      return "";
+  }
+}
+
+function pathToTitle(path: string) {
+  const addendum = createPathAddendum(path);
+  const base = "Matt Kaye";
+
+  if (!addendum) {
+    return base;
+  } else {
+    return `${base} | ${addendum}`;
+  }
+}
+
 export default function App({ Component, pageProps }: AppProps) {
+  const path = usePathname();
+  const title = pathToTitle(path);
+
   return (
-    <>
-      <Head>
-        <title>Matt Kaye</title>
-      </Head>
-      <GoogleAnalytics trackPageViews gaMeasurementId="G-6KV13R4B6Y" />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </>
+    path && (
+      <>
+        <Head>
+          <title>{title}</title>
+        </Head>
+        <GoogleAnalytics trackPageViews gaMeasurementId="G-6KV13R4B6Y" />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </>
+    )
   );
 }
