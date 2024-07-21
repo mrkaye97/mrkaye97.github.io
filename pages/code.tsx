2209f59516608be4e8ce102ec4d7abb8d81c372a
Matt Kaye <mrkaye97@gmail.com>
2024-07-20 22:04:09 -0400

    add a zen screenshot

--- pages/code.tsx
+++ pages/code.tsx
@@ -1,16 +1,33 @@
 import { TextLink } from "@/src/components/links";
+import Image from "next/image";
 import React from "react";
 
 function CodeCard({
   title,
   children,
+  imageUrl,
 }: {
   title: string;
   children: React.ReactNode;
+  imageUrl?: string;
 }) {
   return (
     <div className="bg-opacity-100 border border-gray-500 shadow-2xl rounded-lg p-6 m-4">
-      <h1 className="text-3xl font-bold text-white mb-4">{title}</h1>
+      <h1 className={`text-3xl font-bold text-white ${imageUrl ? "" : "mb-4"}`}>
+        {title}
+      </h1>
+      {imageUrl && (
+        <div className="flex justify-center py-4 md:px-6">
+          <Image
+            src={imageUrl}
+            alt={`${title} screenshot`}
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{ width: "100%", height: "auto" }}
+          />
+        </div>
+      )}
       {children}
     </div>
   );
@@ -19,12 +36,11 @@ function CodeCard({
 export default function Code() {
   return (
     <div className="max-w-3xl mx-auto px-4 py-8">
-      <CodeCard title={"Introduction"}>
+      <CodeCard title="Introduction">
         <p className="text-lg text-white mb-4">
           I like writing code for fun, to explore interesting questions and
           problems, to learn, and to improve the tools I use.
         </p>
-
         <p className="text-lg text-white mb-4">
           I usually have at least one or two things I&apos;m toying around with
           at any given time, whether it&apos;s{" "}
@@ -55,7 +71,7 @@ export default function Code() {
           .
         </p>
       </CodeCard>
-      <CodeCard title="ZenSearch">
+      <CodeCard title="ZenSearch" imageUrl="/zen-home.png">
         <p className="text-lg text-white mb-4">
           <TextLink text="ZenSearch" href="https://zensearch.jobs" /> is a
           NextJS / FastAPI app I&apos;ve been building to make your job hunt
@@ -64,7 +80,6 @@ export default function Code() {
           in posts a new job to their board that matches my criteria{" "}
           <em>without needing to check their job board every day.</em>
         </p>
-
         <p className="text-lg text-white mb-4">
           It&apos;s come a long way since then - our small team now is
           supporting thousands of job seekers looking for jobs at almost ten
