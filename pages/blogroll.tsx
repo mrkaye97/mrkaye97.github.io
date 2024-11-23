2083b51494f2aa010b111916538fecaa5f6e2692
Matt Kaye <mrkaye97@gmail.com>
2024-11-23 14:36:07 -0500

    add categories

--- pages/blogroll.tsx
+++ pages/blogroll.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
-import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
 type BlogPost = {
   url: string;
   title: string;
   author: string;
+  date: string | undefined;
+  categories: string[];
 };
 
 const posts: BlogPost[] = [
@@ -12,76 +13,102 @@ const posts: BlogPost[] = [
     url: "https://www.telkins.dev/blog/do-best-practices-really-matter",
     title: "Do best practices really matter?",
     author: "Trevor Elkins",
+    date: "2024-04-16",
+    categories: ["engineering"],
   },
   {
     url: "https://medium.com/@matryer/line-of-sight-in-code-186dd7cdea88",
     title: "Code: Align the happy path to the left edge",
     author: "Mat Ryer",
+    date: "2016-08-25",
+    categories: ["engineering"],
   },
   {
     url: "https://jamie-wong.com/2013/07/12/grep-test/",
     title: "Too DRY - The Grep Test",
     author: "Jamie Wong",
+    date: "2013-07-12",
+    categories: ["engineering"],
   },
   {
     url: "https://www.depesz.com/2021/10/22/why-is-it-hard-to-automatically-suggest-what-index-to-create/",
     title: "Why is it hard to automatically suggest what index to create?",
     author: "Hubert Lubaczewski",
+    date: "2021-10-22",
+    categories: ["engineering", "databases"],
   },
   {
     url: "https://postgres.ai/blog/20220525-common-db-schema-change-mistakes",
     title: "Common DB schema change mistakes",
     author: "Nikolay Samokhvalov",
+    date: "2022-05-25",
+    categories: ["engineering", "databases"],
   },
   {
     url: "https://johnresig.com/blog/keeping-passwords-in-source-control/",
     title: "Keeping Passwords in Source Control",
     author: "John Resig",
+    date: "2013-02-06",
+    categories: ["engineering"],
   },
   {
     url: "https://www.kalzumeus.com/2012/01/23/salary-negotiation/",
     title: "Salary Negotiation: Make More Money, Be More Valued",
     author: "Patrick McKenzie",
+    date: "2012-01-23",
+    categories: ["career"],
   },
   {
     url: "https://begriffs.com/posts/2018-03-20-user-defined-order.html",
     title: "User-defined Order in SQL",
     author: "Joe Nelson",
+    date: "2018-03-20",
+    categories: ["engineering", "databases"],
   },
   {
     url: "https://docs.hatchet.run/blog/problems-with-celery",
     title: "The problems with (Python's) Celery",
     author: "Alexander Belanger",
+    date: "2024-06-05",
+    categories: ["engineering"],
   },
   {
     url: "https://grugbrain.dev/",
     title:
       "The Grug Brained Developer: A layman's guide to thinking like the self-aware smol brained",
     author: "Anonymous",
+    date: undefined,
+    categories: ["engineering"],
   },
   {
     url: "https://sandimetz.com/blog/2016/1/20/the-wrong-abstraction",
     title: "The Wrong Abstraction",
     author: "Sandi Metz",
+    date: "2016-01-20",
+    categories: ["engineering"],
   },
   {
     url: "https://kentcdodds.com/blog/aha-programming",
     title: "AHA Programming",
     author: "Kent C. Dodds",
+    date: "2020-06-22",
+    categories: ["engineering"],
   },
   {
     url: "https://kentcdodds.com/blog/usememo-and-usecallback",
     title: "When to useMemo and useCallback",
     author: "Kent C. Dodds",
+    date: "2019-06-04",
+    categories: ["engineering", "react"],
   },
 ];
 
 function PostCard({ post }: { post: BlogPost }) {
   return (
     <Link href={post.url} target="_blank">
-      <div className="bg-opacity-100 border border-gray-500 shadow-2xl rounded-lg p-6 w-full transition-transform transform-gpu hover:scale-95 hover:bg-opacity-90">
-        <h2 className="text-3xl font-bold text-white mt-4">{post.title}</h2>
-        <p className="text-xl text-gray-300 mt-2">{post.author}</p>
+      <div className="h-full bg-opacity-100 shadow-2xl rounded-lg p-6 transition-transform transform-gpu hover:scale-95 hover:bg-opacity-90 flex flex-col justify-between bg-blue-500 gap-y-4 min-h-64">
+        <h2 className="text-3xl font-bold text-white">{post.title}</h2>
+        <p className="text-xl text-gray-300">{post.author}</p>
       </div>
     </Link>
   );
@@ -89,16 +116,10 @@ function PostCard({ post }: { post: BlogPost }) {
 
 export default function BlogPostsILike() {
   return (
-    <div className="m-2">
-      <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
-        <Masonry>
-          {posts.map((post) => (
-            <div key={post.title} className="p-1">
-              <PostCard key={post.url} post={post} />
-            </div>
-          ))}
-        </Masonry>
-      </ResponsiveMasonry>
+    <div className="py-8 px-4 md:px-16 grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {posts.map((post) => (
+        <PostCard key={post.url} post={post} />
+      ))}
     </div>
   );
 }
