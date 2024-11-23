0bf277228d1946602866e83f4c6d196ab5cae05b
Matt Kaye <mrkaye97@gmail.com>
2024-11-23 14:26:09 -0500

    pretty tag on posts

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,19 +1,28 @@
+import { Badge } from "@/components/ui/badge";
 import { getSortedPostsData, PostData } from "@/src/common/posts";
 import Link from "next/link";
 
 function PostCard({ postData }: { postData: PostData }) {
   return (
     <Link href={`/blog/${postData.id}`} className="h-full">
-      <div className="h-full bg-opacity-100 shadow-2xl rounded-lg p-6 transition-transform transform-gpu hover:scale-95 hover:bg-opacity-90 flex flex-col justify-between bg-blue-500">
-        <div>
-          <h2 className="text-3xl font-bold text-white mt-4 mx-4">
-            {postData.title}
-          </h2>
-          <p className="text-xl text-gray-300 mx-4 my-2">{postData.preview}</p>
+      <div className="h-full bg-opacity-100 shadow-2xl rounded-lg p-6 transition-transform transform-gpu hover:scale-95 hover:bg-opacity-90 flex flex-col justify-between bg-blue-500 gap-y-4 min-h-64">
+        <div className="flex flex-col gap-y-2">
+          <h2 className="text-3xl font-bold text-white">{postData.title}</h2>
+          <p className="text-xl text-gray-300">{postData.preview}</p>
         </div>
-        <h3 className="text-xl font-bold text-white mx-4 my-2">
-          {postData.date}
-        </h3>
+        <div className="flex flex-col md:flex-row justify-between md:items-center gap-y-2">
+          <p className="text-md font-semibold text-white">{postData.date}</p>
+          <div className="flex flex-row items-center gap-x-1">
+            {postData.categories.map((c) => (
+              <Badge
+                className="bg-light-seafoam text-black hover:bg-bg-light-seafoam"
+                key={c}
+              >
+                {c.toLowerCase()}
+              </Badge>
+            ))}
+          </div>
+        </div>
       </div>
     </Link>
   );
@@ -28,7 +37,7 @@ export default function BlogPosts({ allPosts }: { allPosts: PostData[] }) {
   console.log(tags);
 
   return (
-    <div className="py-8 px-4 md:px-16 grid md:grid-cols-2 lg:grid-cols-3 gap-6 bg-sea">
+    <div className="py-8 px-4 md:px-16 grid md:grid-cols-2 lg:grid-cols-3 gap-4">
       {allPosts.map((postData) => (
         <PostCard key={postData.id} postData={postData} />
       ))}
